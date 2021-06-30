9be49f246ba12b1eae0e3b70378f6716445bd879
Assisneto Damasceno <netodamasceno12@gmail.com>
2021-06-30 17:20:44 -0300

    second class

---
+++ lib/wabanex/users/get.ex
@@ -0,0 +1,20 @@
+defmodule Wabanex.Users.Get do
+  alias Ecto.UUID
+  alias Wabanex.{Repo, User}
+
+  def call(id) do
+    id
+    |> UUID.cast()
+    |> handle_response()
+  end
+
+
+  defp handle_response(:error), do: {:error, "Invalid UUID"}
+
+  defp handle_response({:ok, uuid}) do
+    case Repo.get(User, uuid) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+end
